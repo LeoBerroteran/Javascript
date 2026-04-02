001021a4de271f45c7327a01483454e6538c93a7
LeoBerroteran <leowensberroteranr@gmail.com>
2026-04-02 15:30:59 -0400

    Proyecto
    Necesito ayuda con el ejercicio de piedra papel o tijeras

---
+++ proyecto.js
@@ -0,0 +1,37 @@
+  /* PROYECTO DE LA SECCION 2 1RA PARTE*/
+
+  /* 1) Realiza una funcionalidad que funcione como el juego de Piedra, Papel o Tijera, 
+  el usuario debe ingresar su eleccion, luego la computadora hara una eleccion aleatoria (investigar como generar un numero aleatorio en JavaScript),
+  y se mostrara el resultado de quien gano.*/
+
+  let opciones = ["Piedra", "Papel", "Tijera"];
+  let eleccionUsuario = prompt("Elige: Piedra, Papel o Tijera");
+  let eleccionCPU = opciones[Math.floor(Math.random() * opciones.length)];
+  console.log('Eleccion usuario: ' + eleccionUsuario);
+  console.log('Eleccion CPU: ' + eleccionCPU);
+
+
+  /*2) Realiza una funcionalidad que permita al usuario ingresar un texto, y te muestre el numero de vocales, letras, palabras que tiene ese texto.*/
+
+  let texto = "Javascript es un dolor de cabeza";
+  let vocales = texto.match(/[aeiouAEIOU]/g);
+  let numeroVocales = vocales ? vocales.length : 0;
+  let numeroLetras = texto.replace(/[^a-zA-Z]/g, "").length;
+  let numeroPalabras = texto.trim().split(/\s+/).length;
+  console.log(`El texto tiene ${numeroVocales} vocales, ${numeroLetras} letras y ${numeroPalabras} palabras.`);
+
+
+  /*3) Realiza una funcionalidad que verifique si un string es un palindromo, 
+  es decir, que se lee igual de izquierda a derecha que de derecha a izquierda,
+  por ejemplo "oso" es un palindromo, mientras que "hola" no lo es.*/
+
+  let palindromo = ["Salas" , "Rosa"];
+  for (let i = 0; i < palindromo.length; i++) {
+    let palabra = palindromo[i].toLowerCase();
+    let palabraInvertida = palabra.split("").reverse().join("");
+    if (palabra === palabraInvertida) {
+      console.log(`${palindromo[i]} es un palindromo`);
+    } else {
+      console.log(`${palindromo[i]} no es un palindromo`);
+    }
+  }
